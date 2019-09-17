a960c74eda68886a3e1ca963fc609a9d63c6a73e
李志强 <lizhiqiang1@msunsoft.com>
2019-09-17 22:09:32 +0800

    get all cpu

--- SfxPerf/Program.cs
+++ SfxPerf/Program.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Management;
 using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 
-namespace SfxPerf
+namespace sfxcpu
 {
     class Program
     {
@@ -17,7 +18,7 @@ namespace SfxPerf
         static PerformanceCounter cFree = null;
         static PerformanceCounter AvgDiskQueueLength = null;
         static PerformanceCounter AvgDiskSecRead = null;
-        static PerformanceCounter AvgDiskSecWrite= null;
+        static PerformanceCounter AvgDiskSecWrite = null;
         static PerformanceCounter CurrentDiskQueueLength = null;
         static PerformanceCounter AvgDiskSecTransfer = null;
         static PerformanceCounter BufferCacheHitRatio = null;
@@ -29,6 +30,7 @@ namespace SfxPerf
         static PerformanceCounter LockWaitsSec = null;
         static PerformanceCounter ProcessesBlocked = null;
         static PerformanceCounter CheckpointPagesSec = null;
+        static PerformanceCounter FullScansSec = null;
         //IIS
         static PerformanceCounter iisCurrentConnections = null;
         static PerformanceCounter aspnetRequestsCurrent = null;
@@ -48,6 +50,8 @@ namespace SfxPerf
         static void Main(string[] args)
         {
 
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             Console.WriteLine("开始搜集性能数据.....................");
             init();
             StringBuilder result = new StringBuilder();
@@ -58,26 +62,47 @@ namespace SfxPerf
                 {
                     result.Clear();
                     result.AppendLine("************************************************************");
-                    result.AppendLine("时间:" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
                     GetSystemInfo(result);
                     GetIIS(result);
                     GetSqlServer(result);
                     GetNetwork(result);
+
                     result.AppendLine("************************************************************");
                     Console.WriteLine(result.ToString());
                     File.AppendAllText("sfxperf.txt", result.ToString());
                     Thread.Sleep(1000);
-                }catch(Exception ex)
+                }
+                catch (Exception ex)
                 {
-                    Console.WriteLine(ex.Message+ex.StackTrace);
+                    Console.WriteLine(ex.Message + ex.StackTrace);
                     return;
                 }
-               
-            }
 
+            }
+            //while (true)
+            //{
+            //    try
+            //    {
+
+            //        GetAllCpu();
+            //        Thread.Sleep(1000);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //       // Console.WriteLine(ex.Message + ex.StackTrace);
+            //        //File.AppendAllText("err.txt", ex.Message + ex.StackTrace);
+            //    }
+            //}
 
 
         }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+
+        }
+
         static void init()
         {
             try
@@ -134,6 +159,10 @@ namespace SfxPerf
                 {
                     ReCompilationsSec = new PerformanceCounter("SQLServer:SQL Statistics", "Re-Compilations/Sec");
                 }
+                if (PerformanceCounterCategory.Exists("SQLServer:Access Methods") && PerformanceCounterCategory.CounterExists("Full Scans/sec", "SQLServer:Access Methods"))
+                {
+                    FullScansSec = new PerformanceCounter("SQLServer:Access Methods", "Full Scans/sec");
+                }
                 if (PerformanceCounterCategory.Exists("SQLServer:General Statistics") && PerformanceCounterCategory.CounterExists("User Connections", "SQLServer:General Statistics"))
                 {
                     UserConnections = new PerformanceCounter("SQLServer:General Statistics", "User Connections");
@@ -167,7 +196,7 @@ namespace SfxPerf
                 {
                     aspnetRequestsQueued = new PerformanceCounter("ASP.NET", "Requests Queued");
                 }
-                
+
                 #endregion
                 if (PerformanceCounterCategory.Exists("TCPv4") && PerformanceCounterCategory.CounterExists("Connections Established", "TCPv4"))
                 {
@@ -198,7 +227,8 @@ namespace SfxPerf
                         NetworkInterface networkInterface = allNetworkInterfaces[i];
                         if (networkInterface.OperationalStatus == OperationalStatus.Up
                             && (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Ethernet
-                                ||networkInterface.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                                || networkInterface.NetworkInterfaceType == NetworkInterfaceType.GigabitEthernet
+                                )
                                 )
                         {
                             nis.Add(networkInterface.Description);
@@ -225,12 +255,12 @@ namespace SfxPerf
                         }
                     }
                 }
-                
+
 
             }
             catch (Exception ex)
             {
-                Console.WriteLine("init失败"+ex.Message + ex.StackTrace);
+                Console.WriteLine("init失败" + ex.Message + ex.StackTrace);
             }
         }
         static void GetSystemInfo(StringBuilder result)
@@ -248,11 +278,11 @@ namespace SfxPerf
             {
                 result.AppendLine("C盘剩余空间率: " + cFree.NextValue() + "%");
             }
-            if(AvgDiskQueueLength != null)
+            if (AvgDiskQueueLength != null)
             {
-                result.AppendLine("AvgDiskQueueLength: " + AvgDiskQueueLength.NextValue()+ ",理想值队列长度	小于 2");
+                result.AppendLine("AvgDiskQueueLength: " + AvgDiskQueueLength.NextValue() + ",理想值队列长度	小于 2");
             }
-            if(CurrentDiskQueueLength != null)
+            if (CurrentDiskQueueLength != null)
             {
                 result.AppendLine("CurrentDiskQueueLength: " + CurrentDiskQueueLength.NextValue() + ",理想值队列长度	小于 2");
             }
@@ -262,7 +292,7 @@ namespace SfxPerf
             }
             if (AvgDiskSecRead != null)
             {
-                result.AppendLine("AvgDiskSecRead: " + AvgDiskSecRead.NextValue()+ ",一般不要超过11～15ms");
+                result.AppendLine("AvgDiskSecRead: " + AvgDiskSecRead.NextValue() + ",一般不要超过11～15ms");
             }
             if (AvgDiskSecWrite != null)
             {
@@ -271,13 +301,13 @@ namespace SfxPerf
         }
         static void GetSqlServer(StringBuilder result)
         {
-            if(LockWaitsSec == null)
+            if (LockWaitsSec == null)
             {
                 result.AppendLine("====无数据库服务====");
                 return;
             }
             result.AppendLine("=======数据库=========");
-            
+
             if (BufferCacheHitRatio != null)
             {
                 result.AppendLine("BufferCacheHitRatio: " + BufferCacheHitRatio.NextValue() + "%");
@@ -290,7 +320,10 @@ namespace SfxPerf
             {
                 result.AppendLine("BatchRequestsSec: " + BatchRequestsSec.NextValue());
             }
-
+            if (FullScansSec != null)
+            {
+                result.AppendLine("FullScansSec: " + FullScansSec.NextValue());
+            }
             if (SQLCompilationsSec != null)
             {
                 result.AppendLine("SQLCompilationsSec: " + SQLCompilationsSec.NextValue());
@@ -307,6 +340,7 @@ namespace SfxPerf
             {
                 result.AppendLine("LockWaitsSec: " + LockWaitsSec.NextValue() + ",要保持这个计数器为零或接近零。");
             }
+
             if (ProcessesBlocked != null)
             {
                 result.AppendLine("ProcessesBlocked: " + ProcessesBlocked.NextValue());
@@ -335,8 +369,8 @@ namespace SfxPerf
         }
         static void GetNetwork(StringBuilder result)
         {
-         
-            
+
+
             result.AppendLine("=====获取网络数据=====");
             if (tcpConnectionsEstablished != null)
             {
@@ -358,7 +392,7 @@ namespace SfxPerf
             {
                 result.AppendLine("ConnectionFailures 失败的TCP连接数: " + ConnectionFailures.NextValue());
             }
-            
+
             //if (currentBideWith != null)
             //{
             //    result.AppendLine("currentBideWith 当前带宽: " + currentBideWith.NextValue());
@@ -367,7 +401,7 @@ namespace SfxPerf
             float bideWith = 0;
             if (nics != null)
             {
-               
+
                 foreach (var c in nics)
                 {
                     //result.AppendLine(c.CounterHelp);
@@ -387,7 +421,7 @@ namespace SfxPerf
                     //    result.AppendLine(c.CounterName + "(" + c.InstanceName + "):" + c.NextValue().ToString("#"));
                     //}
                 }
-                result.AppendLine("网络使用率%:" + ((total*8)/bideWith)*100);
+                result.AppendLine("网络使用率%:" + ((total * 8) / bideWith) * 100);
             }
             if (nics2 != null)
             {
@@ -409,12 +443,12 @@ namespace SfxPerf
                     //    result.AppendLine(c.CounterName + "(" + c.InstanceName + "):" + c.NextValue().ToString("#"));
                     //}
                 }
-                result.AppendLine("网络使用率%:" + ((total * 8) / bideWith)*100);
+                result.AppendLine("网络使用率%:" + ((total * 8) / bideWith) * 100);
             }
 
 
         }
-        
+
         public static string GetInstanceName(string categoryName, string counterName, Process p)
         {
             try
@@ -506,6 +540,134 @@ namespace SfxPerf
 
 
         }
+        static void GetAllCpu()
+        {
 
+            using (ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher("SELECT Name,ExecutablePath, KernelModeTime, UserModeTime,WorkingSetSize,WorkingSetSize,ThreadCount,CommandLine,ProcessId FROM Win32_Process"))
+            {
+                foreach (ManagementBaseObject managementBaseObject in managementObjectSearcher.Get())
+                {
+                    try
+                    {
+                        ManagementObject managementObject = (ManagementObject)managementBaseObject;
+                        int ProcessId = Convert.ToInt32(managementObject["ProcessId"]);
+                        //System.Diagnostics.Trace.WriteLine(managementObject["UserModeTime"]);
+                        long userModeTime = Convert.ToInt64(managementObject["UserModeTime"]);
+                        long KernelModeTime = Convert.ToInt64(managementObject["KernelModeTime"]);
+                        string processName = managementObject["Name"].ToString();
+                        string commandLine = managementObject["CommandLine"] == null ? "" : managementObject["CommandLine"].ToString();
+                        StringBuilder result = new StringBuilder();
+                        if (KernelModeTime > 0 && userModeTime > 0)
+                        {
+                            string percent = getCpuPercent(ProcessId);
+                            if (percent != "" && percent != "0")
+                            {
+
+
+                                result.Append("**************************************************\r\n");
+
+                                result.AppendLine("时间:" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                                result.Append("进程名称：").AppendLine(managementObject["Name"].ToString());
+                                result.Append("命令行：").AppendLine(commandLine);
+                                result.Append("CPU占用率:").Append(percent).AppendLine("%");
+                                result.Append("**************************************************\r\n");
+                                Console.WriteLine(result.ToString());
+                                File.AppendAllText("sfxcpu.txt", result.ToString());
+                            }
+                            else
+                            {
+                                Console.Write(".");
+                            }
+                        }
+
+
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //Console.WriteLine(ex.Message + ex.StackTrace);
+                        //File.AppendAllText("err.txt", ex.Message + ex.StackTrace);
+                    }
+
+                }
+
+
+            }
+        }
+        static string getCpuPercent(string ProcessName)
+        {
+
+            string percent = "";
+            try
+            {
+                using (var p1 = new PerformanceCounter("Process", "% Processor Time", Path.GetFileNameWithoutExtension(ProcessName)))
+                {
+
+                    percent = (p1.NextValue() / Environment.ProcessorCount).ToString();
+                    Thread.Sleep(200);
+                    percent = (p1.NextValue() / Environment.ProcessorCount).ToString();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
+                File.AppendAllText("err.txt", ex.Message + ex.StackTrace);
+            }
+            return percent;
+
+        }
+        static string getCpuPercent(int pid)
+        {
+            string percent = "";
+
+            var pro = Process.GetProcessById(pid);
+            if (pro.HasExited) return "";
+
+            {
+
+                //间隔时间（毫秒）
+
+                int interval = 100;
+
+                //上次记录的CPU时间
+
+                var prevCpuTime = TimeSpan.Zero;
+
+
+
+                //当前时间
+
+                var curTime = pro.TotalProcessorTime;
+
+                //间隔时间内的CPU运行时间除以逻辑CPU数量
+
+                var value = (curTime - prevCpuTime).TotalMilliseconds / interval / Environment.ProcessorCount * 100;
+
+                prevCpuTime = curTime;
+
+                //输出
+
+                Thread.Sleep(interval);
+                var pro1 = Process.GetProcessById(pid);
+                if (pro.HasExited) return "";
+                {
+                    curTime = pro.TotalProcessorTime;
+                    value = (curTime - prevCpuTime).TotalMilliseconds / interval / Environment.ProcessorCount * 100;
+
+                }
+                percent = value.ToString();
+
+            }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message + ex.StackTrace);
+            //    File.AppendAllText("err.txt", ex.Message + ex.StackTrace);
+            //}
+            return percent;
+        }
     }
+
 }
